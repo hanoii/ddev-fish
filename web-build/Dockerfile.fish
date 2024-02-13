2159c23d23d9bc49c9635043101941fc6b043c2b
Ariel Barreiro <abarrei@gmail.com>
2024-02-13 14:43:36 -0300

    Initial addition of fish

---
+++ web-build/Dockerfile.fish
@@ -0,0 +1,4 @@
+#ddev-generated
+RUN echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+RUN curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+RUN apt update && apt satisfy -y "fish (>=3.7)"
